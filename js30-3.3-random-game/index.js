6254aaf6cea1b6c06722d6142ff005d8767e1335
GarryProhor <igorprohorchenko@gmail.com>
2023-10-01 20:20:40 -0800

    feat: js drawing player

--- js30-3.3-random-game/index.js
+++ js30-3.3-random-game/index.js
@@ -4,9 +4,24 @@ let boardWidth = 600;
 let boardHeight = 600;
 let context;
 
+//игрок
+let playerWidth = 80;
+let playerHeight = 10;
+
+let player = {
+    x: boardWidth/2 - playerWidth/2,
+    y: boardHeight - playerHeight - 5,
+    width: playerWidth,
+    height: playerHeight
+}
+
 window.onload = function () {
     board = document.querySelector('.board');
     board.width = boardWidth;
     board.height = boardHeight;
     context = board.getContext('2d'); // рисуем доску
+
+    //рисуем игрока
+    context.fillStyle = 'deepskyblue';
+    context.fillRect(player.x, player.y, player.width, player.height);
 }
